5990b5c5b1f9585c8223274b3d08aa6830dc5b18
MarinJ <mjuric00@fesb.hr>
2022-04-09 22:31:35 +0200

    Start working on the create user domain implementation

--- src/main/kotlin/marinj/feature/auth/domain/usecase/CreateUser.kt
+++ src/main/kotlin/marinj/feature/auth/domain/usecase/CreateUser.kt
@@ -5,7 +5,7 @@ import marinj.core.model.Failure
 import marinj.feature.auth.domain.repository.AuthRepository
 
 class CreateUser(
-    private val authRepository: AuthRepository
+    private val authRepository: AuthRepository,
 ) {
 
     suspend operator fun invoke(
@@ -13,7 +13,7 @@ class CreateUser(
         userName: String?,
         password: String?,
     ): Either<Failure, Int> {
-        //TODO add validation and remove double bang
+
 
         return authRepository.createUser(
             email = email!!,
