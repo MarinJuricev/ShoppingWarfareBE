3372596bcd00ee5c2239726f6ab415802dbb048f
MarinJ <mjuric00@fesb.hr>
2022-04-13 00:17:05 +0200

    Add the first draft for GenerateToken and connect the lifetime for access and refresh tokens through the config file

--- src/main/kotlin/marinj/core/config/ShoppingWarfareConfig.kt
+++ src/main/kotlin/marinj/core/config/ShoppingWarfareConfig.kt
@@ -7,6 +7,8 @@ private const val JWT_ISSUER = "jwt.issuer"
 private const val JWT_AUDIENCE = "jwt.audience"
 private const val JWT_REALM = "jwt.realm"
 private const val JWT_CLAIM = "jwt.claim"
+private const val JWT_ACCESS_LIFETIME = "jwt.access.lifetime"
+private const val JWT_REFRESH_LIFETIME = "jwt.refresh.lifetime"
 
 data class ShoppingWarfareConfig(
     val jwtConfig: JwtConfig
@@ -18,6 +20,8 @@ data class JwtConfig(
     val audience: String,
     val realm: String,
     val claim: String,
+    val accessTokenLifeTime: Long,
+    val refreshTokenLifeTime: Long,
 )
 
 fun buildJwtConfig(applicationConfig: ApplicationConfig): JwtConfig =
@@ -27,5 +31,7 @@ fun buildJwtConfig(applicationConfig: ApplicationConfig): JwtConfig =
         audience = applicationConfig.property(JWT_AUDIENCE).getString(),
         realm = applicationConfig.property(JWT_REALM).getString(),
         claim = applicationConfig.property(JWT_CLAIM).getString(),
+        accessTokenLifeTime = applicationConfig.property(JWT_ACCESS_LIFETIME).getString().toLong(),
+        refreshTokenLifeTime = applicationConfig.property(JWT_REFRESH_LIFETIME).getString().toLong(),
     )
 
