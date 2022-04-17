d9a11b0689d93f83705caf6378f1833890cb1e0a
marinjuricev <mjuric00@fesb.hr>
2022-04-17 21:41:35 +0200

    Rename the authModule to Account module

--- src/main/kotlin/marinj/feature/account/di/AccountModule.kt
+++ src/main/kotlin/marinj/feature/account/di/AccountModule.kt
@@ -5,9 +5,13 @@ import com.auth0.jwt.JWTCreator
 import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
 import marinj.core.config.ShoppingWarfareConfig
+import marinj.feature.account.data.dao.Tokens
+import marinj.feature.account.data.dao.TokensDao
 import marinj.feature.account.data.dao.Users
 import marinj.feature.account.data.dao.UsersDao
+import marinj.feature.account.data.repository.TokenRepositoryImpl
 import marinj.feature.account.data.repository.UserRepositoryImpl
+import marinj.feature.account.domain.repository.TokenRepository
 import marinj.feature.account.domain.repository.UserRepository
 import marinj.feature.account.domain.usecase.CreateUser
 import marinj.feature.account.domain.usecase.GenerateToken
@@ -17,14 +21,23 @@ import org.koin.dsl.module
 
 val authModule = module {
     single<UsersDao> { Users }
+    single<TokensDao> { Tokens }
+
     factory<UserRepository> {
         UserRepositoryImpl(
             usersDao = get(),
         )
     }
+    factory<TokenRepository> {
+        TokenRepositoryImpl(
+            tokensDao = get(),
+        )
+    }
+
     factory {
         CreateUser(
             userRepository = get(),
+            tokenRepository = get(),
         )
     }
     factory {
