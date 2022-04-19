31455b1f3d6f751e1dee0750df7499ad050b57bc
MarinJ <mjuric00@fesb.hr>
2022-04-20 00:30:45 +0200

    Connect AccountInitializer to accountService logic

--- src/main/kotlin/marinj/feature/account/infrastructure/AccountInitializer.kt
+++ src/main/kotlin/marinj/feature/account/infrastructure/AccountInitializer.kt
@@ -1,15 +1,13 @@
 package marinj.feature.account.infrastructure
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.JWTVerifier
-import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.auth.Authentication
-import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.jwt.jwt
 import marinj.core.config.ShoppingWarfareConfig
 import marinj.core.di.inject
+import marinj.core.model.Either.Left
+import marinj.core.model.Either.Right
 import marinj.feature.account.infrastructure.service.AccountService
 
 fun Application.installAuth() {
@@ -22,21 +20,13 @@ fun Application.installAuth() {
             realm = config.jwtConfig.secret
             verifier(accountService.jwtVerifier)
             validate { credential ->
-                //TODO actually configure this to get the userId from the service
-                if (credential.payload.getClaim("id").asString() != "") {
-                    JWTPrincipal(credential.payload)
-                } else {
-                    null
+                val accessToken = credential.payload.getClaim(config.jwtConfig.claim).toString()
+
+                when (val result = accountService.validateAccessToken(accessToken)) {
+                    is Right -> result.value
+                    is Left -> null
                 }
             }
-//            validate { // 3
-//                val payload = it.payload
-//                val claim = payload.getClaim("id")
-//                val claimString = claim.asInt()
-//                val user = db.findUser(claimString) // 4
-//                user
-//            }
         }
     }
-
 }
--- src/main/kotlin/marinj/feature/account/infrastructure/service/AccountService.kt
+++ src/main/kotlin/marinj/feature/account/infrastructure/service/AccountService.kt
@@ -2,10 +2,14 @@ package marinj.feature.account.infrastructure.service
 
 import com.auth0.jwt.JWTVerifier
 import marinj.core.model.Either
-import marinj.core.model.Either.*
+import marinj.core.model.Either.Left
+import marinj.core.model.Either.Right
 import marinj.core.model.Failure
+import marinj.core.model.buildRight
 import marinj.feature.account.domain.usecase.CreateUser
+import marinj.feature.account.domain.usecase.ValidateAccessToken
 import marinj.feature.account.infrastructure.model.ResponseToken
+import marinj.feature.account.infrastructure.model.ResponseToken.Companion.mapFromDomain
 
 interface AccountService {
 
@@ -23,6 +27,7 @@ interface AccountService {
 
 class AccountServiceImpl(
     private val createUserUseCase: CreateUser,
+    private val validateAccessTokenUseCase: ValidateAccessToken,
     override val jwtVerifier: JWTVerifier,
 ) : AccountService {
 
@@ -32,13 +37,15 @@ class AccountServiceImpl(
         userName: String?,
     ): Either<Failure, ResponseToken> =
         when (val result = createUserUseCase(email, password, userName)) {
-            is Right -> TODO()
+            is Right -> result.value.mapFromDomain().buildRight()
             is Left -> result
         }
 
     override suspend fun validateAccessToken(
         accessToken: String
-    ): Either<Failure, ResponseToken> {
-        TODO("Not yet implemented")
-    }
+    ): Either<Failure, ResponseToken> =
+        when (val result = validateAccessTokenUseCase(accessToken)) {
+            is Right -> result.value.mapFromDomain().buildRight()
+            is Left -> result
+        }
 }
